801abc291de877f87e51cffe544cbbbdb3dd0ae8
Peter Papadimitropoulos <ppapadim12@gmail.com>
2025-03-05 12:50:27 -0500

    add Nav and Hero components to App

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <script setup lang="ts">
-import Nav from "./components/Nav.vue";
+  import Nav from './components/nav/Nav.vue';
+  import Hero from './components/hero/Hero.vue';
+  import Calculator from './components/calculator/Calculator.vue';
+
+  import { nav, hero } from './data';
 </script>
 
 <template>
   <header>
-    <Nav />
+    <Nav v-bind="nav" />
   </header>
+  <main>
+    <section class="hero-section bg-primary-green text-white">
+      <div class="width-wrapper">
+        <Hero v-bind="hero" />
+      </div>
+    </section>
+    <section>
+      <Calculator />
+    </section>
+  </main>
 </template>
 
 <style scoped>
+  .hero-section {
+    padding: 30px 24px;
+    text-align: center;
+    height: 280px;
+  }
+
+  @media (min-width: 768px) {
+    .width-wrapper {
+      max-width: 600px;
+      margin: 0 auto;
+    }
+  }
 </style>
