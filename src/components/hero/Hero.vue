1d33b6523c7c3700b7b11276ae20b54a2cada549
Peter Papadimitropoulos <ppapadim12@gmail.com>
2025-03-05 12:30:07 -0500

    move Hero.vue & create types

---
+++ src/components/hero/Hero.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import type { HeroProps } from './hero.types';
+  defineProps<HeroProps>();
+</script>
+
+<template>
+  <h1 class="bold">{{ title }}</h1>
+  <h2 class="regular">{{ description }}</h2>
+</template>
+
+<style scoped>
+  h1 {
+    font-size: 16px;
+    margin-bottom: 15px;
+  }
+
+  h2 {
+    font-size: 16px;
+  }
+</style>
