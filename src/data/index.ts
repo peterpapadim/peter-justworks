3c39b4a83617cd5848910eb2eb9e3663b14a4986
Peter Papadimitropoulos <ppapadim12@gmail.com>
2025-03-05 12:28:26 -0500

    create data source

---
+++ src/data/index.ts
@@ -0,0 +1,18 @@
+export const nav = {
+  logo: {
+    label: 'CoinCalc',
+  },
+  links: [
+    { label: 'github', href: 'https://www.github.com' },
+    {
+      label: 'codepeter',
+      href: 'https://github.com/peterpapadim/peter-justworks',
+    },
+  ],
+};
+
+export const hero = {
+  title: 'Convert USD to Cryptocurrency',
+  description:
+    'Begin by entering the amount in USD that you wish to convert. Choose up to five cryptocurrencies and allocate your funds among your selections.',
+};
