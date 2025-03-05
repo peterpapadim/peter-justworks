0ad1afcf54a2abfb3de7e3e56a78436603990fa2
Peter Papadimitropoulos <ppapadim12@gmail.com>
2025-03-05 12:50:06 -0500

    init calculator component

---
+++ src/components/calculator/Calculator.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import DollarInput from './DollarInput.vue';
+
+  const dollarAmount = ref(100);
+  const setDollarAmount = (value: number) => {
+    dollarAmount.value = value;
+  };
+</script>
+
+<template>
+  <form>
+    <DollarInput @set-dollar-amount="setDollarAmount" />
+  </form>
+</template>
+
+<style scoped></style>
