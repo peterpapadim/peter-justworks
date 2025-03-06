6a3ffe99fa0c7c3f969addf9ab5679f5978f44ae
Peter Papadimitropoulos <ppapadim12@gmail.com>
2025-03-05 23:27:55 -0500

    create reusable AddCard

---
+++ src/components/calculator/AddCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import type { AddCardProps } from './calculator.types';
+  defineProps<AddCardProps>();
+</script>
+
+<template>
+  <button type="button" @click="onClick" class="text-primary-green medium">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+  button {
+    height: 100%;
+    width: 100%;
+    cursor: pointer;
+    background-color: transparent;
+    border: none;
+    font-size: 16px;
+   }
+</style>
