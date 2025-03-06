fe7d600f2ec390666fdb4747c5da43928cb7adfc
Peter Papadimitropoulos <ppapadim12@gmail.com>
2025-03-05 23:26:28 -0500

    create reusable CurrencyDropdown

---
+++ src/components/calculator/CurrencyDropdown.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import type { CurrencyDropdownProps } from './calculator.types';
+  const { selectedOption, options, onChange, id } =
+    defineProps<CurrencyDropdownProps>();
+  const selected = ref<string>(selectedOption);
+</script>
+
+<template>
+  <label class="visually-hidden" :for="id">Select a currency</label>
+  <div class="select-wrapper">
+    <select :id="id" v-model="selected" @change="onChange">
+      <option v-for="option in options" :key="option">{{ option }}</option>
+    </select>
+  </div>
+</template>
+
+<style scoped>
+  .select-wrapper {
+    width: 150px;
+  }
+  select {
+    font-size: 36px;
+  }
+</style>
